896e985ad2fdc035695b3d81a156a962f8e36371
Andy Ta <me.andyta@gmail.com>
2020-11-28 18:58:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Retrospective
+
+Collaborative sticky notes for sprint retrospectives. 
+
+Used the [Fluid Angular Brainstorm Demo](https://fluidframework.com/playground/?path=/story/welcome--page) as a starter base.
+
+Step to run the app:
+
+1. `yarn install`
+2. `yarn start`
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Notero } from '../services/notero.data-object';
+import { INote, INoteWithVotes, IUser } from '../shared/interfaces';
+
+@Component({
+  selector: 'app-board',
+  template: `
+    <div class="board">
+      <div *ngFor="let note of notes">
+        <app-note
+          [note]="note"
+          [count]="note.votes"
+          [user]="user"
+          [highlightMine]="highlightMine"
+          (click)="vote(note)"></app-note>
+      </div>
+    </div>
+  `
+})
+export class BoardComponent implements OnInit {
+
+  @Input() model: Notero;
+  @Input() notes: INoteWithVotes[];
+  @Input() user: IUser;
+  @Input() highlightMine: any;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  vote(note: INote) {
+    this.model.vote(note);
+  }
+
+}
